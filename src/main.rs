8cd2204750940da38aa51f4da6a6b63410cd3f23
Teddy Wing <git@teddywing.com>
2016-08-05 21:07:18 -0400

    Test terminal output
    Output a single string to the terminal in the alternate screen. This
    allows us to test the base functionality of what we'll need for a dialog
    a la URLView.
    
    Used Rustty's examples as a big reference.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate rustty;
+
+use rustty::{Terminal, Event};
+use rustty::ui::Painter;
+
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut term = Terminal::new().unwrap();
+    term.swap_buffers().unwrap();
+
+    loop {
+        term.printline(0, 0, "booyakacha");
+
+        let evt = term.get_event(Duration::from_millis(100)).unwrap();
+        if let Some(Event::Key(ch)) = evt {
+            match ch {
+                'q' => {
+                    break;
+                }
+                c @ _ => {
+                }
+            }
+        }
+
+        term.swap_buffers().unwrap();
+    }
 }
