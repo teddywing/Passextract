5e433fb0d12198301e2a05727899b7312a31e47e
Teddy Wing <git@teddywing.com>
2016-08-05 21:53:38 -0400

    Allow both 'q' and Ctrl-C to quit
    Alo update the title text with information on how to quit.

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     term.swap_buffers().unwrap();
 
     loop {
-        term.printline(0, 0, "booyakacha");
+        term.printline(0, 0, "Passextract (Press q or Ctrl-C to quit)");
 
         let evt = term.get_event(Duration::from_millis(100)).unwrap();
         if let Some(Event::Key(ch)) = evt {
             match ch {
-                'q' => {
+                'q' | '\x03' => {
                     break;
                 }
                 c @ _ => {
