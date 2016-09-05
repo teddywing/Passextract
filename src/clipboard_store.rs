c1950f087ec24e947ce739875e181ca2ff69f21e
Teddy Wing <git@teddywing.com>
2016-09-05 02:58:10 -0400

    ClipboardStore: Rename "last", only set it on first run
    * Rename `last` to `original` because we only want to set it the first
      time we set the clipboard. Subsequent times we set the clipboard
      contents should not affect this saved value.
    * Only set `original` on first run of the clipboard `set_contents`
      function.

--- src/clipboard_store.rs
+++ src/clipboard_store.rs
@@ -6,7 +6,7 @@ use clipboard::ClipboardContext;
 
 pub struct ClipboardStore {
     context: ClipboardContext,
-    last: String,
+    original: String,
 }
 
 impl ClipboardStore {
@@ -16,15 +16,15 @@ impl ClipboardStore {
         return Ok(
             ClipboardStore {
                 context: context,
-                last: String::new(),
+                original: String::new(),
             }
         )
     }
 
     pub fn set_contents(&mut self, data: String) -> Result<(), Box<Error>> {
-        // Save last value
-        self.last = try!(self.context.get_contents());
-        println!("last: {}", self.last);
+        if self.original.is_empty() {
+            self.original = try!(self.context.get_contents())
+        }
 
         // Set new clipboard contents
         // self.context.set_contents(data)
